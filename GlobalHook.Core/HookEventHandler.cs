c9a9e7889e9398436ddac5d9b3630a78508392d6
Kir-Antipov <kp_antipov@mail.ru>
2020-05-10 17:01:38 +0300

    Made contravariant replacement for EventHandler

---
+++ GlobalHook.Core/HookEventHandler.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace GlobalHook.Core
+{
+    public delegate void HookEventHandler(object sender, EventArgs e);
+
+    public delegate void HookEventHandler<in TEventArgs>(object sender, TEventArgs e) where TEventArgs : IHookEventArgs;
+
+    public delegate void HookEventHandler<in THook, in TEventArgs>(THook sender, TEventArgs e) where THook : IHook where TEventArgs : IHookEventArgs;
+}
