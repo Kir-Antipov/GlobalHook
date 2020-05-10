98e7137f3546daa0f1e087e189c4c32fa9903764
Kir-Antipov <kp_antipov@mail.ru>
2020-05-10 17:04:30 +0300

    Made interface for hook's EventArgs

---
+++ GlobalHook.Core/IHookEventArgs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace GlobalHook.Core
+{
+    public interface IHookEventArgs
+    {
+        bool DefaultPrevented { get; }
+
+        void PreventDefault();
+
+        bool CanDefaultBePrevented { get; }
+
+        SenderType SenderType { get; }
+
+        DateTime Time { get; }
+    }
+}
