0ab07bb71b3483e052a5c355b5c98a63e795c11d
Kir-Antipov <kp_antipov@mail.ru>
2020-05-10 17:05:25 +0300

    Made basic interface for hook

---
+++ GlobalHook.Core/IHook.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace GlobalHook.Core
+{
+    public interface IHook : IDisposable
+    {
+        bool CanBeInstalled { get; }
+
+        void Install();
+
+        void Install(long threadId);
+
+        void Uninstall();
+
+        event HookEventHandler<IHookEventArgs>? OnEvent;
+    }
+}
