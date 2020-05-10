70859abffb577fd7ee86486dac008d979858a72a
Kir-Antipov <kp_antipov@mail.ru>
2020-05-10 18:12:37 +0300

    Added interface for mouse hook

---
+++ GlobalHook.Core/Mouse/IMouseEventArgs.cs
@@ -0,0 +1,26 @@
+﻿using GlobalHook.Core.Keyboard;
+
+namespace GlobalHook.Core.Mouse
+{
+    public interface IMouseEventArgs : IHookEventArgs
+    {
+        MouseEventType MouseEventType { get; }
+
+        IPoint Coords { get; }
+
+        int Delta { get; }
+
+        MouseButtons Key { get; }
+        KeyState KeyState { get; }
+
+        /// <summary>
+        /// If <see cref="MouseEventType"/> == <see cref="MouseEventType.Key"/>,
+        /// <see cref="Key"/> == <see cref="MouseButtons.Left"/> and
+        /// <see cref="KeyState"/> == <see cref="KeyState.Down"/>,
+        /// then this property indicates whether the click was second in a row
+        /// </summary>
+        bool IsDoubleClick { get; }
+
+        SenderType IHookEventArgs.SenderType => SenderType.Mouse;
+    }
+}
