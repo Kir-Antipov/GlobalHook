2306d500f642cfbb7221e7220395e63b14123430
Kir-Antipov <kp_antipov@mail.ru>
2020-05-10 17:53:16 +0300

    Added interface for keyboard hook

---
+++ GlobalHook.Core/Keyboard/IKeyboardHook.cs
@@ -0,0 +1,9 @@
+﻿namespace GlobalHook.Core.Keyboard
+{
+    public interface IKeyboardHook : IHook
+    {
+        event HookEventHandler<IHookEventArgs>? IHook.OnEvent { add => OnEvent += value; remove => OnEvent -= value; }
+
+        new event HookEventHandler<IKeyboardEventArgs>? OnEvent;
+    }
+}
