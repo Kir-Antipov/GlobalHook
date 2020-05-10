69ccaf6a761366686a6096f75b922b7b22c8387a
Kir-Antipov <kp_antipov@mail.ru>
2020-05-10 17:52:51 +0300

    Made EventArgs interface for keyboard events

---
+++ GlobalHook.Core/Keyboard/IKeyboardEventArgs.cs
@@ -0,0 +1,10 @@
+﻿namespace GlobalHook.Core.Keyboard
+{
+    public interface IKeyboardEventArgs : IHookEventArgs
+    {
+        Keys Key { get; }
+        KeyState KeyState { get; }
+
+        SenderType IHookEventArgs.SenderType => SenderType.Keyboard;
+    }
+}
