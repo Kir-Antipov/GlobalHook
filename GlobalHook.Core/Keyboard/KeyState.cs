717204d347fd4a29d4c49665a6d1144fae3ad8d5
Kir-Antipov <kp_antipov@mail.ru>
2020-05-10 17:52:15 +0300

    Added `KeyState` enum

---
+++ GlobalHook.Core/Keyboard/KeyState.cs
@@ -0,0 +1,18 @@
+﻿namespace GlobalHook.Core.Keyboard
+{
+    public enum KeyState
+    {
+        None    = 0b000000000,
+        Down    = 0b100000000,
+        Up      = 0b100000001,
+        SysDown = 0b100000100,
+        SysUp   = 0b100000101
+    }
+
+    public static class KeyStateExtensions
+    {
+        public static bool IsUp(this KeyState state) => state == KeyState.Up || state == KeyState.SysUp;
+
+        public static bool IsDown(this KeyState state) => state == KeyState.Down || state == KeyState.SysDown;
+    }
+}
