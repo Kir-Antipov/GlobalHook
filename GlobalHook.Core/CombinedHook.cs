191a3d3a9d54bdb66a94742c07c3ac53551d0fb8
Kir-Antipov <kp_antipov@mail.ru>
2020-05-11 05:46:58 +0300

    Added some helper methods to IHook

---
+++ GlobalHook.Core/CombinedHook.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GlobalHook.Core
+{
+    internal class CombinedHook : IHook
+    {
+        public bool CanBeInstalled => true;
+
+        public event HookEventHandler<IHookEventArgs>? OnEvent;
+
+        private readonly IHook[] Hooks;
+
+        public CombinedHook(IEnumerable<IHook> hooks)
+        {
+            Hooks = hooks.Where(x => x.CanBeInstalled).ToArray();
+            Array.ForEach(Hooks, x => x.OnEvent += (sender, args) => OnEvent?.Invoke(sender, args));
+        }
+
+        public void Install() => Array.ForEach(Hooks, x => x.Install());
+
+        public void Install(long threadId) => Array.ForEach(Hooks, x => x.Install(threadId));
+
+        public void Uninstall() => Array.ForEach(Hooks, x => x.Uninstall());
+
+        public void Dispose()
+        {
+            Uninstall();
+            GC.SuppressFinalize(this);
+        }
+
+        ~CombinedHook() => Uninstall();
+    }
+}
--- GlobalHook.Core/IHook.cs
+++ GlobalHook.Core/IHook.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
 
 namespace GlobalHook.Core
 {
@@ -13,5 +17,62 @@ namespace GlobalHook.Core
         void Uninstall();
 
         event HookEventHandler<IHookEventArgs>? OnEvent;
+
+        public static IHook Combine(params IHook[] loggers) => new CombinedHook(loggers);
+
+        public static IHook Combine(IEnumerable<IHook> loggers) => new CombinedHook(loggers);
+
+        public static IEnumerable<IHook> Load(Assembly assembly) => assembly
+            .ExportedTypes
+            .Where(typeof(IHook).IsAssignableFrom).Where(x => !x.IsInterface && !x.IsAbstract)
+            .Select(x => x.GetConstructor(Type.EmptyTypes) is { } constructor ? (IHook)constructor.Invoke(null) : null)
+            .Where(x => x is { })!;
+
+        public static IEnumerable<IHook> Load(string directory, string searchPattern, EnumerationOptions options)
+        {
+            if (Directory.Exists(directory))
+                return LoadFromDirectory(directory, searchPattern, options);
+
+            throw new DirectoryNotFoundException(directory);
+        }
+
+        public static IEnumerable<IHook> Load(string directory, string searchPattern, SearchOption options) => Load(directory, searchPattern, options == SearchOption.AllDirectories ? new EnumerationOptions { RecurseSubdirectories = true } : new EnumerationOptions());
+
+        public static IEnumerable<IHook> Load(string directory, string searchPattern) => Load(directory, searchPattern, new EnumerationOptions());
+        
+        public static IEnumerable<IHook> Load(string path)
+        {
+            if (File.Exists(path))
+                return LoadFromFile(path);
+
+            if (Directory.Exists(path))
+                return LoadFromDirectory(path, "*.dll", new EnumerationOptions());
+
+            throw new FileNotFoundException(path);
+        }
+
+        public static IHook LoadCombined(Assembly assembly) => Combine(Load(assembly));
+
+        public static IHook LoadCombined(string path) => Combine(Load(path));
+
+        public static IHook LoadCombined(string directory, string searchPattern) => Combine(Load(directory, searchPattern));
+
+        public static IHook LoadCombined(string directory, string searchPattern, EnumerationOptions options) => Combine(Load(directory, searchPattern, options));
+
+        public static IHook LoadCombined(string directory, string searchPattern, SearchOption options) => Combine(Load(directory, searchPattern, options));
+
+        private static IEnumerable<IHook> LoadFromFile(string path)
+        {
+            try
+            {
+                return Load(Assembly.Load(File.ReadAllBytes(path)));
+            }
+            catch
+            {
+                return Enumerable.Empty<IHook>();
+            }
+        }
+
+        private static IEnumerable<IHook> LoadFromDirectory(string path, string searchPattern, EnumerationOptions options) => Directory.EnumerateFiles(path, searchPattern, options).SelectMany(LoadFromFile);
     }
 }
