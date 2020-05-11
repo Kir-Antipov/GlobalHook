1e758689bee11ffe7639451a826dbebd77b149a6
Kir-Antipov <kp_antipov@mail.ru>
2020-05-11 18:55:56 +0300

    Added static wrapper for kernel32.dll

---
+++ GlobalHook.Core.Windows/Interop/Libs/Kernel32.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace GlobalHook.Core.Windows.Interop.Libs
+{
+    internal static class Kernel32
+    {
+        public const string LibraryName = "kernel32.dll";
+
+        private static readonly DateTime PCStarted;
+
+        [DllImport(LibraryName, SetLastError = true)]
+        public static extern IntPtr LoadLibrary(string libraryName);
+
+        [DllImport(LibraryName)]
+        public extern static long GetTickCount64();
+
+        [DllImport(LibraryName, CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern IntPtr GetModuleHandle(string? moduleName);
+
+        public static DateTime TicksToDateTime(double ticks) => PCStarted.AddMilliseconds(ticks);
+
+        static Kernel32() => PCStarted = DateTime.Now.AddMilliseconds(-GetTickCount64());
+    }
+}
