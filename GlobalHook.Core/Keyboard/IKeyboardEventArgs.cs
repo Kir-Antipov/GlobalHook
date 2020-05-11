d8f7d192c107f829d017a7f8af4d7b9328114033
Kir-Antipov <kp_antipov@mail.ru>
2020-05-11 05:09:16 +0300

    Added more specific events to the keyboard hook interface

--- GlobalHook.Core/Keyboard/IKeyboardEventArgs.cs
+++ GlobalHook.Core/Keyboard/IKeyboardEventArgs.cs
@@ -5,6 +5,11 @@
         Keys Key { get; }
         KeyState KeyState { get; }
 
+        /// <summary>
+        /// If event was raised by character key, gets the character value.
+        /// </summary>
+        char? KeyChar { get; }
+
         SenderType IHookEventArgs.SenderType => SenderType.Keyboard;
     }
 }
--- GlobalHook.Core/Keyboard/IKeyboardHook.cs
+++ GlobalHook.Core/Keyboard/IKeyboardHook.cs
@@ -5,5 +5,31 @@
         event HookEventHandler<IHookEventArgs>? IHook.OnEvent { add => OnEvent += value; remove => OnEvent -= value; }
 
         new event HookEventHandler<IKeyboardEventArgs>? OnEvent;
+
+        /// <summary>
+        /// Occurs when a key is pressed. 
+        /// </summary>
+        event HookEventHandler<IKeyboardEventArgs>? KeyDown;
+
+        /// <summary>
+        /// Occurs when a key is released. 
+        /// </summary>
+        event HookEventHandler<IKeyboardEventArgs>? KeyUp;
+
+        /// <summary>
+        /// Occurs when a key is pressed.
+        /// </summary>
+        /// <remarks>
+        /// Key events occur in the following order: 
+        /// <list type="number">
+        /// <item><see cref="KeyDown"/></item>
+        /// <item><see cref="KeyPress"/></item>
+        /// <item><see cref="KeyUp"/></item>
+        /// </list>
+        /// The <see cref="KeyPress"/> event is not raised by noncharacter keys; however, the noncharacter keys do raise 
+        /// the <see cref="KeyDown"/> and <see cref="KeyUp"/> events. 
+        /// Use the <see cref="IKeyboardEventArgs.KeyChar"/> property to sample keystrokes at run time and to consume a subset of common keystrokes. 
+        /// </remarks>
+        event HookEventHandler<IKeyboardEventArgs>? KeyPress;
     }
 }
