cdfaf1296aebcd7be667cdaad49185bbf86b73fb
Kir-Antipov <kp_antipov@mail.ru>
2020-05-12 18:45:57 +0300

    Added `quit` parameter to `goOnPredicate`

--- GlobalHook.Core/MessageLoop/IMessageLoop.cs
+++ GlobalHook.Core/MessageLoop/IMessageLoop.cs
@@ -13,50 +13,54 @@ namespace GlobalHook.Core.MessageLoop
         /// </summary>
         /// <param name="hooks">Hooks to be launched.</param>
         /// <param name="threadId">The identifier of the target thread.</param>
-        /// <param name="goOnPredicate">Callback that indicates whether to continue (<see langword="true"/>) the message loop.</param>
-        void Run(IEnumerable<IHook> hooks, long threadId, Func<bool> goOnPredicate);
+        /// <param name="goOnPredicate">
+        /// Callback that indicates whether to continue (<see langword="true"/>) the message loop.
+        /// <para/>
+        /// Its parameter describes whether an exit from the loop itself has been requested.
+        /// </param>
+        void Run(IEnumerable<IHook> hooks, long threadId, Func<bool, bool> goOnPredicate);
 
-        /// <inheritdoc cref="Run(IEnumerable{IHook}, long, Func{bool})"/>
-        void Run(IEnumerable<IHook> hooks, Func<bool> goOnPredicate);
+        /// <inheritdoc cref="Run(IEnumerable{IHook}, long, Func{bool, bool})"/>
+        void Run(IEnumerable<IHook> hooks, Func<bool, bool> goOnPredicate);
 
         /// <summary>
         /// This is an aggregated event for all running hooks.
         /// </summary>
-        event HookEventHandler<IHookEventArgs>? OnEvent;
+        event EventHandler<IHookEventArgs>? OnEvent;
     }
 
     public static class MessageLoopExtensions
     {
-        /// <inheritdoc cref="IMessageLoop.Run(IEnumerable{IHook}, Func{bool})"/>
+        /// <inheritdoc cref="IMessageLoop.Run(IEnumerable{IHook}, Func{bool, bool})"/>
         /// <param name="loop">Message loop instance.</param>
-        public static void Run(this IMessageLoop loop, long threadId, Func<bool> goOnPredicate, params IHook[] hooks) => loop.Run(hooks, threadId, goOnPredicate);
+        public static void Run(this IMessageLoop loop, long threadId, Func<bool, bool> goOnPredicate, params IHook[] hooks) => loop.Run(hooks, threadId, goOnPredicate);
 
-        /// <inheritdoc cref="Run(IMessageLoop, long, Func{bool}, IHook[])"/>
-        public static void Run(this IMessageLoop loop, Func<bool> goOnPredicate, params IHook[] hooks) => loop.Run(hooks, goOnPredicate);
+        /// <inheritdoc cref="Run(IMessageLoop, long, Func{bool, bool}, IHook[])"/>
+        public static void Run(this IMessageLoop loop, Func<bool, bool> goOnPredicate, params IHook[] hooks) => loop.Run(hooks, goOnPredicate);
 
-        /// <inheritdoc cref="Run(IMessageLoop, long, Func{bool}, IHook[])"/>
+        /// <inheritdoc cref="Run(IMessageLoop, long, Func{bool, bool}, IHook[])"/>
         /// <param name="cancellationToken">The message <paramref name="loop"/> will be stopped immediately after the cancel request for this token.</param>
-        public static void Run(this IMessageLoop loop, IEnumerable<IHook> hooks, long threadId, CancellationToken cancellationToken) => loop.Run(hooks, threadId, () => !cancellationToken.IsCancellationRequested);
+        public static void Run(this IMessageLoop loop, IEnumerable<IHook> hooks, long threadId, CancellationToken cancellationToken) => loop.Run(hooks, threadId, quit => !(quit || cancellationToken.IsCancellationRequested));
         
         /// <inheritdoc cref="Run(IMessageLoop, IEnumerable{IHook}, long, CancellationToken)"/>
-        public static void Run(this IMessageLoop loop, IEnumerable<IHook> hooks, CancellationToken cancellationToken) => loop.Run(hooks, () => !cancellationToken.IsCancellationRequested);
+        public static void Run(this IMessageLoop loop, IEnumerable<IHook> hooks, CancellationToken cancellationToken) => loop.Run(hooks, quit => !(quit || cancellationToken.IsCancellationRequested));
 
         /// <inheritdoc cref="Run(IMessageLoop, IEnumerable{IHook}, long, CancellationToken)"/>
-        public static void Run(this IMessageLoop loop, long threadId, CancellationToken cancellationToken, params IHook[] hooks) => loop.Run(hooks, threadId, () => !cancellationToken.IsCancellationRequested);
+        public static void Run(this IMessageLoop loop, long threadId, CancellationToken cancellationToken, params IHook[] hooks) => loop.Run(hooks, threadId, quit => !(quit || cancellationToken.IsCancellationRequested));
         
         /// <inheritdoc cref="Run(IMessageLoop, IEnumerable{IHook}, long, CancellationToken)"/>
-        public static void Run(this IMessageLoop loop, CancellationToken cancellationToken, params IHook[] hooks) => loop.Run(hooks, () => !cancellationToken.IsCancellationRequested);
+        public static void Run(this IMessageLoop loop, CancellationToken cancellationToken, params IHook[] hooks) => loop.Run(hooks, quit => !(quit || cancellationToken.IsCancellationRequested));
 
-        /// <inheritdoc cref="Run(IMessageLoop, long, Func{bool}, IHook[])"/>
-        public static void Run(this IMessageLoop loop, long threadId, params IHook[] hooks) => loop.Run(hooks, threadId, () => true);
+        /// <inheritdoc cref="Run(IMessageLoop, long, Func{bool, bool}, IHook[])"/>
+        public static void Run(this IMessageLoop loop, long threadId, params IHook[] hooks) => loop.Run(hooks, threadId, quit => !quit);
 
-        /// <inheritdoc cref="Run(IMessageLoop, long, Func{bool}, IHook[])"/>
-        public static void Run(this IMessageLoop loop, IEnumerable<IHook> hooks, long threadId) => loop.Run(hooks, threadId, () => true);
+        /// <inheritdoc cref="Run(IMessageLoop, long, Func{bool, bool}, IHook[])"/>
+        public static void Run(this IMessageLoop loop, IEnumerable<IHook> hooks, long threadId) => loop.Run(hooks, threadId, quit => !quit);
 
-        /// <inheritdoc cref="Run(IMessageLoop, long, Func{bool}, IHook[])"/>
-        public static void Run(this IMessageLoop loop, params IHook[] hooks) => loop.Run(hooks, () => true);
+        /// <inheritdoc cref="Run(IMessageLoop, long, Func{bool, bool}, IHook[])"/>
+        public static void Run(this IMessageLoop loop, params IHook[] hooks) => loop.Run(hooks, quit => !quit);
 
-        /// <inheritdoc cref="Run(IMessageLoop, long, Func{bool}, IHook[])"/>
-        public static void Run(this IMessageLoop loop, IEnumerable<IHook> hooks) => loop.Run(hooks, () => true);
+        /// <inheritdoc cref="Run(IMessageLoop, long, Func{bool, bool}, IHook[])"/>
+        public static void Run(this IMessageLoop loop, IEnumerable<IHook> hooks) => loop.Run(hooks, quit => !quit);
     }
 }
