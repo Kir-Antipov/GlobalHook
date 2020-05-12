af18e0be6689f5418397726e7dd70ab1638afde7
Kir-Antipov <kp_antipov@mail.ru>
2020-05-12 21:06:49 +0300

    CombinedHook fix

--- GlobalHook.Core/CombinedHook.cs
+++ GlobalHook.Core/CombinedHook.cs
@@ -11,18 +11,20 @@ namespace GlobalHook.Core
         public event EventHandler<IHookEventArgs>? OnEvent;
 
         private readonly IHook[] Hooks;
+        private readonly EventHandler<IHookEventArgs> Delegate;
 
         public CombinedHook(IEnumerable<IHook> hooks)
         {
+            Delegate = (sender, args) => OnEvent?.Invoke(sender, args);
             Hooks = hooks.Where(x => x.CanBeInstalled).ToArray();
-            Array.ForEach(Hooks, x => x.OnEvent += (sender, args) => OnEvent?.Invoke(sender, args));
+            Array.ForEach(Hooks, x => x.OnEvent += Delegate);
         }
 
         public void Install() => Array.ForEach(Hooks, x => x.Install());
 
         public void Install(long threadId) => Array.ForEach(Hooks, x => x.Install(threadId));
 
-        public void Uninstall() => Array.ForEach(Hooks, x => x.Uninstall());
+        public void Uninstall() => Array.ForEach(Hooks, x => { x.OnEvent -= Delegate; x.Uninstall(); });
 
         public void Dispose()
         {
